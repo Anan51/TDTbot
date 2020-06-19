1f198519ba66e00084301b3f049a85c545a895a2
StellarNemesis <come@mebro.email>
2020-06-19 17:28:46 -0400

    Made debugging functions hidden and added git_pull, reload_cogs

--- cogs/debugging.py
+++ cogs/debugging.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+import inspect
 from ..helpers import *
+from .. import git_manage
 
 
 class Debugging(commands.Cog):
@@ -17,24 +19,24 @@ class Debugging(commands.Cog):
             return True
         return False
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def RuntimeError(self, ctx):
         """Raise a runtime error (because why not)"""
         raise RuntimeError("Per user request")
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def flush(self, ctx, n: int = 10):
         """<n=10 (optional)> flushes stdout with n newlines"""
         print('\n' * n)
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def reboot(self, ctx):
         """Reboots this bot"""
         await ctx.send("Ok. I will reboot now.")
         print('\nRebooting\n\n\n\n')
         await self.bot.loop.run_until_complete(self.bot.logout())
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def channel_id(self, ctx, channel: str = None, guild: str = None):
         """<channel (optional)> <server (optional)> sends random roast message"""
         if guild is None:
@@ -51,7 +53,7 @@ class Debugging(commands.Cog):
             channel = ctx.channel
         await ctx.send('Channel "{0.name}" has id {0.id}.'.format(channel))
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def member_id(self, ctx, member: str = None, guild: str = None):
         """<channel (optional)> <server (optional)> sends random roast message"""
         if guild is None:
@@ -67,6 +69,22 @@ class Debugging(commands.Cog):
         else:
             member = ctx.author
         await ctx.send('{0.name} has id {0.id}.'.format(member))
+
+    @commands.command(hidden=True)
+    async def git_pull(self, ctx):
+        """Do a git pull on own code"""
+        git_manage.update()
+
+
+    @commands.command(hidden=True)
+    async def reload_cogs(self, ctx, option=None):
+        """Reloads all cogs that were added as extensions"""
+        if option == 'pull':
+            await self.git_pull(ctx)
+        msg = 'Reloading ' + ', '.join([i.split('.')[-1] for i in self.bot.extensions])
+        for i in self.bot.extensions:
+            self.bot.reload_extension(i)
+        await ctx.send(msg.rstrip(', '))
 
 
 def setup(bot):
