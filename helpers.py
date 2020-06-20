7b65bec545456ca9afaab5b4f434b203bdb2266e
StellarNemesis <come@mebro.email>
2020-06-20 09:36:56 -0400

    Helpers now return None if they can't find channel/role

--- helpers.py
+++ helpers.py
@@ -4,8 +4,14 @@ from .param import rc as _rc
 def find_channel(guild, name=None):
     if name is None:
         name = _rc('channel')
-    return [i for i in guild.channels if i.name.lower() == name.lower()][0]
+    try:
+        return [i for i in guild.channels if i.name.lower() == name.lower()][0]
+    except IndexError:
+        return
 
 
 def find_role(guild, name):
-    return [i for i in guild.roles if i.name.lower() == name.lower()][0]
+    try:
+        return [i for i in guild.roles if i.name.lower() == name.lower()][0]
+    except IndexError:
+        return
