3403f529af8270a7f15e9cc532de0570d616a161
StellarNemesis <come@mebro.email>
2020-06-25 07:46:33 -0400

    Bug fix attempt for member join

--- cogs/alerts.py
+++ cogs/alerts.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from .. import param
 from ..helpers import *
 
 
@@ -10,7 +11,7 @@ class Alerts(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = member.guild.get_channel('debugging')
+        channel = self.bot.find_channel(param.rc('log_channel'))
         roles = [find_role(member.guild, i) for i in ["Admin", "Devoted"]]
         roles = " ".join([i.mention for i in roles if hasattr(i, 'mention')])
         print(member, roles, channel)
