931893d21309ebcb8f0a667510cf183796139370
StellarNemesis <come@mebro.email>
2020-06-26 21:09:43 -0400

    documentation

--- cogs/alerts.py
+++ cogs/alerts.py
@@ -5,12 +5,14 @@ from ..helpers import *
 
 
 class Alerts(commands.Cog):
+    """Cog to listen and send alerts"""
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        """Alert admin type roles on new member joining"""
         channel = self.bot.find_channel(param.rc('log_channel'))
         roles = [find_role(member.guild, i) for i in ["Admin", "Devoted"]]
         roles = " ".join([i.mention for i in roles if hasattr(i, 'mention')])
