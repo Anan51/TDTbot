cb6e393383733d18f4eff64df7cd5f9cb26c5b80
StellarNemesis <come@mebro.email>
2021-04-14 14:15:49 -0400

    added data handling to lfg cog

--- cogs/lfg.py
+++ cogs/lfg.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import datetime
+import pytz
 import pickle
 import shelve
 import logging
@@ -12,6 +14,9 @@ from ..async_helpers import *
 logger = logging.getLogger('discord.' + __name__)
 _dbm = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
 _dbm = os.path.join(_dbm, 'config', 'lfg.dbm')
+_tmax = datetime.timedelta(days=90)
+_role2emoji = {'pc': 'steam'}
+_tz = pytz.timezone(param.rc('timezone'))
 
 
 class _ActivityFile:
@@ -40,6 +45,49 @@ class _ActivityFile:
 
     def items(self):
         return self.file.items()
+
+    def _clear_old(self, key):
+        old = (datetime.datetime.utcnow() - _tmax).timestamp()
+        if key in self:
+            pops = [i for i in self[key] if i[1] < old]
+            for p in pops:
+                self[key].pop(p)
+
+    def update_pings(self, msg, roles):
+        user_id = msg.author.id
+        self._clear_old(user_id)
+        entry = [msg.id, msg.created_at.timestamp()] + roles
+        if user_id in self:
+            self[user_id].append(entry)
+        else:
+            self[user_id] = [entry]
+
+    def _fmt_row(self, row):
+        msg = row[0]
+        # assign UTC timezone
+        dt = pytz.utc.localize(datetime.datetime.fromtimestamp(row[1]))
+        # convert to server timezone
+        dt = dt.astimezone(_tz)
+        roles = ' '.join([':{}:'.format(_role2emoji.get(i, i)) for i in row[2:]])
+        return roles + ' {}; message ID: {}'.format(dt, msg)
+
+    def member_list(self, mid):
+        out = []
+        if mid in self:
+            out = [self._fmt_row(i) for i in self[mid]]
+        return out
+
+    def member_summary(self, mid):
+        data = self[mid]
+        tot = len(data)
+        roles = dict()
+        for d in data:
+            for role in d[2:]:
+                if role in roles:
+                    roles[role] += 1
+                else:
+                    roles[role] = 1
+        return tot, roles
 
 
 class LFG(commands.Cog):
@@ -74,15 +122,35 @@ class LFG(commands.Cog):
         # if not lfg channel
         if message.channel.id != 560270058224615425:
             return
-        role_tagged = False
+        roles_tagged = []
         for role in message.role_mentions:
             if role.name.endswith(' D2'):
                 name = role.name[:-3].lower()
                 # role -> emoji
-                emoji = {'pc': 'steam'}.get(name, name)
+                emoji = _role2emoji.get(name, name)
                 emoji = [e for e in message.guild.emojis if e.name == emoji][0]
                 await message.add_reaction(emoji)
-                role_tagged = True
+                roles_tagged.append(name)
+        if roles_tagged:
+            self.data.update_pings(message, roles_tagged)
+
+    @commands.command()
+    @commands.check(admin_check)
+    async def ping_data(self, ctx, member: discord.User = None):
+        if member is None:
+            member = ctx.author
+        await split_send(ctx, self.data.member_list(member.id))
+
+    @commands.command()
+    @commands.check(admin_check)
+    async def ping_summary(self, ctx, member: discord.User = None):
+        if member is None:
+            member = ctx.author
+        tot, data = self.data.member_summary(member.id)
+        out = 'Total: {}; '.format(tot)
+        out += ', '.join(['{} :{}:'.format(value, _role2emoji.get(role, role)) for
+                          role, value in data.items()])
+        await ctx.send(out)
 
 
 def setup(bot):
