c52d668fd6bedfa7150f439e011300a7cf27ef82
StellarNemesis <come@mebro.email>
2021-06-26 12:00:56 -0400

    Added twitter auth stuff

--- twitter.py
+++ twitter.py
@@ -5,9 +5,44 @@ import logging
 
 logger = logging.getLogger('discord.' + __name__)
 
+_auth_file = os.path.split(os.path.realpath(__file__))[0]
+_auth_file = os.path.join(_auth_file, 'config', 'twitter_auth.txt')
+
+
+class AuthKeys:
+    def __init__(self, fn):
+        self.fn = fn
+        with open(_auth_file) as f:
+            lines = [l.split('#')[0].strip() for l in f.readlines()]
+            lines = [l for l in lines if l]
+        self.api_key = lines[0]
+        self.api_secret = lines[1]
+        self.bearer = lines[2]
+        self.access_token = lines[3]
+        self.access_secret = lines[4]
+
+    @property
+    def consumer_key(self):
+        return self.api_key
+
+    @property
+    def consumer_secret(self):
+        return self.api_secret
+
+    @property
+    def consumer(self):
+        return self.consumer_key, self.consumer_secret
+
+    @property
+    def access(self):
+        return self.access_token, self.access_secret
+
+
+_auth_keys = AuthKeys(_auth_file)
+
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler(*_auth_keys.consumer)
+auth.set_access_token(*_auth_keys.access)
 
 # Create API object
 api = tweepy.API(auth)
