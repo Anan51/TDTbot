e230d78e9d5f855c5365e8615411aacd12845cc4
StellarNemesis <come@mebro.email>
2022-09-28 23:47:16 +0000

    Halo => Minecraft

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -61,10 +61,8 @@ class Welcome(commands.Cog):
     """Cog to listen and send alerts"""
     # emoji: role
     _emoji_dict = {878802171913732118: roles.destiny_2,
-                   "🎮": roles.d2_console,
-                   "🖥️": roles.d2_pc,
                    878807665038491668: roles.apex,
-                   921452928055791718: roles.halo,
+                   878806389399625789: roles.minecraft,
                    }
 
     def __init__(self, bot):
