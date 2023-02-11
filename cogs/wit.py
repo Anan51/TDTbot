8155b614649d2260913bfe3a32162691567ccc75
StellarNemesis <come@mebro.email>
2023-02-10 19:43:45 -0500

    Undermine counterfit, add more text stuff

--- cogs/wit.py
+++ cogs/wit.py
@@ -37,7 +37,9 @@ class Wit(commands.Cog, command_attrs=dict(hidden=True)):
 
     @property
     def gold(self):
-        return self._gold
+        return self._gold if self._gold else ":gold:"
+
+# blind_beggar
 
     @commands.command()
     async def blind_beggar(self, ctx):
@@ -61,8 +63,131 @@ class Wit(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.command()
     async def blind_hostile(self, ctx):
         msg = """You deftly draw your weapon on the man. Despite the weapon making no noise the man reacts immediately "wait!... I m-mean." You brandish your weapon aggressively at him, "What a sorry excuse of a human, faking blindness. You disgust me. Get out of here! Go on!" The man springs to his feet and scrambles away in a hurry, leaving 2 coins behind (+2 """
-        msg += (self.gold+" )") if self.gold else ":gold:)"
+        msg += (self.gold+" )")
         await ctx.send(msg)
+
+# fracturing_cliff
+
+    @commands.command()
+    async def fracturing_cliff(self, ctx):
+        msg = """**__Fracturing Cliff__**
+- The Reach suddenly shifts underneath you. It sounds like a massive forest all being snapped in half at once... the edge of the bone cliff is fracturing underneath your feet!
+**RUN!** tdt$cliff_run
+**Hunker Down** tdt$cliff_hunker
+🦊 Fenric: ||**Dash Across the Rubble** tdt$cliff_dash||"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def cliff_run(self, ctx):
+        msg = """You manage to reach the other side of the porous white cliff face. In your haste you may have pulled an ankle. ( 🎲 < 5: Remove ⚡ from a move for the rest of your run)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def cliff_hunker(self, ctx):
+        msg = """You cling to the side of the cliff face, the bone shatters, splints, and falls away around you. You are mostly unscathed except for a few shards that cut into your back. ( 🎲 < 4: -1 ❤️ )"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def cliff_dash(self, ctx):
+        msg = """You nimbly sprint along the cliff face and make it safely to the other side. You're feeling pretty good about yourself (Gain +1 🎲 on all random encounter rolls)"""
+        await ctx.send(msg)
+
+# Blood Puddle
+    @commands.command()
+    async def blood_puddle(self, ctx):
+        msg = """**__Blood Puddle__**
+- As you continue through the forest, you come to a small grove of trees that are unlike the rest. These trees have red leaves and trunks and they are particular swarmed with insects... more than normal. You breach the thicket and find a puddle of blood. You remember hearing that that Giant's Marrow is a rare resource.
+**Collect Some Giant's Blood** tdt$blood_collect
+**Leave the Clearing** tdt$blood_leave
+🦷 Giant: ||**Drink from the Pool** tdt$blood_drink|| """
+        await ctx.send(msg)
+
+    @commands.command()
+    async def blood_collect(self, ctx):
+        msg = """You grab one of your empty water satchels and draw close to the pool, but before you can even get the lid off your satchel you are attacked! ( 🎲 < 8: use tdt$reach_2 | 🎲 > 9: +5 <:gold:1058304371940655185>)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def blood_leave(self, ctx):
+        msg = """You decide to leave the pool alone and let nature figure out what to do with it"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def blood_drink(self, ctx):
+        msg = """You hesitantly cup your hands and drink from the pool. It taste bitter but you feel it resonate with your pulse. (gain +❤️ and +🔷)"""
+        await ctx.send(msg)
+
+# Clingweed
+
+    @commands.command()
+    async def clingweed(self, ctx):
+        msg = """**__Clingweed__**
+- The bone has fallen away here. Previous travelers have set up a rudimentary bridge to cross the gap. You begin carefully crossing over the pit when something sticky brushes against your leg. You try to pull your leg through it but the more you struggle the more it sticks to your flesh. Your movement awakens more of this Clingweed to your presence and its reaching tendrils are closing in on your position FAST.
+**Sever the Vines** tdt$cling_cut
+**Relax your Body** tdt$cling_relax
+🌿 Earthen: ||**Command the Plant** tdt$cling_command||"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def cling_cut(self, ctx):
+        msg = """You draw your weapon and destroy the tendrils attaching to your flesh. ( 🎲 < 4: Lose your passive for the rest of the run | 🎲 > 5: Lose 3 🔷)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def cling_relax(self, ctx):
+        msg = """You conclude the plant is drawn to motion and struggling. You try to relax. The plants reach towards you slows!.. but it doesnt stop. Before you realize it you are surrounded with vines each with a sticky sap like substance on their leaflets you attempt to fight back but its too late, your are slowly being pulled down against the bridge. ( 🎲 < 7: a teammate may spend 1 🔷 for you to try again otherwise: lose your passive for the rest of the run)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def cling_command(self, ctx):
+        msg = """You speak an ancient language to the clingweed. It obeys. You command it to wrap one of its blooms around your back for protection... it wants to obey, but its malnourished. (You may spend 6 🔷 to upgrade your Earthen passive to: __Blooming__ 🌸 "Whenever you __Heal__ recover MP as well" for the rest of the run)"""
+        await ctx.send(msg)
+
+# Shin Splints
+
+    @commands.command()
+    async def shin_splints(self, ctx):
+        msg = """**__Shin Splints__**
+- You are rather mindlessly walking along the worn path. The sun is beating down against your head through the ravine of trees. You are keeping your eyes on the path but when you looks back up you see a field of bone-forged spears planted and fastened into the ground. The spears are pointed towards your approach, and they look sharp.
+**Squeeze Past Them** tdt$shin_squeeze
+**Swat Them out of the Way** tdt$shin_swat
+📚 Educated: ||**Remove a Spear** tdt$shin_remove||"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def shin_squeeze(self, ctx):
+        msg = """You carefully sidle through the field of spears, a few of them catch your clothing. ( 🎲 < 3: -2 ❤️ | 🎲 > 4: -2 <:gold:1058304371940655185>)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def shin_attack(self, ctx):
+        msg = """You Raise your weapon and begin smashing through the spears. Splinters fly in every direction and a few fly towards your face ( 🎲 < 7: -2 ❤️)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def shin_remove(self, ctx):
+        msg = """You squat down along the first row of spears, they are crudely made BUT not flimsily made. You recognize a few of the markings and knots used on the fastenings; they are fenric. "That makes sense" you say out loud to yourself. You think you might be able to remove one of the spears ( 🎲 < 6: failure | 🎲 > 7: Acquire one **Relentless Fang** from the Loot Table)"""
+        await ctx.send(msg)
+
+#    @commands.command()
+#    async def _(self, ctx):
+#        msg = """_"""
+#        await ctx.send(msg)
+#
+#    @commands.command()
+#    async def _(self, ctx):
+#        msg = """_"""
+#        await ctx.send(msg)
+#
+#    @commands.command()
+#    async def _(self, ctx):
+#        msg = """_"""
+#        await ctx.send(msg)
+#
+#    @commands.command()
+#    async def _(self, ctx):
+#        msg = """_"""
+#        await ctx.send(msg)
 
 
 if usingV2:
