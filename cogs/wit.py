00c5ccb070cbdabdf7bc2f5c09c4d932fa7ccbca
StellarNemesis <come@mebro.email>
2023-06-13 20:05:45 -0400

    Added "Quarantined Zone" underwash Wit event

--- cogs/wit.py
+++ cogs/wit.py
@@ -685,6 +685,11 @@ Behavior: +2 to all dice rolls (including speed ties)
 **Run for it** tdt$flood_run
 **Find an Off Shoot** tdt$flood_branch
 :tooth:  Giant: ||**Brace yourself** tdt$flood_brace||""",
+            """**__Quarantined Zone__**
+- The underwash has interesting smells. Often times its a sweet but stale smell from the blue liquid, other times its the faint scent of sewage, and other still its a slight hint of herbs and bread. This time however, it smells like chemicals. The type of chemicals only used in a few places, most of them surrounding something dead. The scent is growing stronger as you are stopped by a large blocked off street. Personnel dot the borders of a large yellow and white dome with yellow paper tape crisscrossing the entrances reading "DANGER BIO HAZARD"
+**Go around** tdt$zone_around
+**Sneak Through** tdt$zone_sneak
+:herb:  Earthen: ||**Release Sunshine** tdt$zone_sun||""",
         ]
         await ctx.send(random.choice(encounters))
 
@@ -790,6 +795,21 @@ Behavior: Select a random player at the start of the game. This will only ever a
     @commands.command()
     async def flood_brace(self, ctx):
         msg = "You eye up the water coming towards you and glance behind you to see a drain isnt far off. You scan your environment and see there is a drain above you but the tunnel is too small for you to fit. You decide your best bet is to grab the ladder above and brace for impact. The water is intense, but nothing you cant handle. Despite it being murky, the water smells like only algae inhabits it, and its cool against your skin. (Gain +1 :large_blue_diamond:)"
+        await ctx.send(msg)
+
+    @commands.command()
+    async def zone_around(self, ctx):
+        msg = "You decided to follow the orders and detour around the site. Unfortunately this will set you back pretty severely. (Go back 5 levels)"
+        await ctx.send(msg)
+
+    @commands.command()
+    async def zone_sneak(self, ctx):
+        msg = "You dont have time to waste. You wait for the staff to look away then you cross under the tape and head inside the dome. Inside black and pinkish sludge are growing along the walls. The smell of chemical has fully given way to the smell of rot. You instinctively cover your face. Bodies are strewn about the zone, with some of their corpses slowly climbing up the walls from the sludge. The room in here is well lit with florescent lamps that seem to be dissipating some of the sludge where it is most concentrated leaving a large amorphous scab where it has dried up. Some site containment crews are looking at equipment and scanning the room with strange devices. Others are collecting samples. The longer you stand in here the more you begin to feel light headed... You head for the door.. (:game_die: < 3 : summon 2 Reach enemies | :game_die: > 4 : lose 1 max HP and 1 max MP, gain one stack of __Will__)"
+        await ctx.send(msg)
+
+    @commands.command()
+    async def zone_sun(self, ctx):
+        msg = "You peer through the sheets of the dome's door and see that they are using high intensity lights to clear the infection. You have a bit of sunlight saved from the Reach, you decide to release it here to help them out (Spend 4 :large_blue_diamond:)"
         await ctx.send(msg)
 
     @commands.command()
