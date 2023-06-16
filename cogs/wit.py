71e17ce6614f3d6882782566f223fb64464335e4
StellarNemesis <come@mebro.email>
2023-06-16 14:50:30 -0400

    Added "Rusted Factory" Wit event

--- cogs/wit.py
+++ cogs/wit.py
@@ -690,6 +690,11 @@ Behavior: +2 to all dice rolls (including speed ties)
 **Go around** tdt$zone_around
 **Sneak Through** tdt$zone_sneak
 :herb:  Earthen: ||**Release Sunshine** tdt$zone_sun||""",
+            """**__Rusted Factory__**
+- The man on the street corner swears that through this factory is a shortcut to the other side of town. Being that a large stone wall separates you from it, you figure you might as well give it a shot. The factory has long since been abandoned. The smell or rust, oil, and hard water saturates through the breeze... ah... a breeze! Maybe the man was right. You flip an old switch and the factory groans to life again. Lightbulbs buzz and flicker illuminating... most... of the factory. Unfortunately the man did not mention that the hole in the wall is higher up on an inaccessible third floor. You are going to have to get creative...
+**Climb Up** tdt$factory_climb
+**Find Another Around** tdt$factory_around
+:mechanical_arm:   Sentian: ||**Examine the Machines** tdt$factory_examine||""",
         ]
         await ctx.send(random.choice(encounters))
 
@@ -810,6 +815,31 @@ Behavior: Select a random player at the start of the game. This will only ever a
     @commands.command()
     async def zone_sun(self, ctx):
         msg = "You peer through the sheets of the dome's door and see that they are using high intensity lights to clear the infection. You have a bit of sunlight saved from the Reach, you decide to release it here to help them out (Spend 4 :large_blue_diamond:)"
+        await ctx.send(msg)
+
+    @commands.command()
+    async def factory_climb(self, ctx):
+        msg = "You let out a sigh. You knew the shortcut was too good to be true. You loosen your shoulders and start mantling equipment and support beams to climb towards the hole. The metal lurches and creaks with every next step... Maybe... maybe you shouldnt be doing this( Gain +1 to your dice for each :zap: move you have: :game_die: < 6 : fall, -1 :heart: and gain :x_ray: __**Fractured**__: \"Skip your first turn every new combat\"  | :game_die: > 7 : skip ahead 3 levels)"
+        await ctx.send(msg)
+
+    @commands.command()
+    async def factory_around(self, ctx):
+        msg = """"This isnt worth it" you mutter to yourself out loud. You turn around to leave the factory when you are greeted by a band of Sentian marauders. They are rusted and brandishing discarded parts. They do NOT look happy to see you. (Fight 3 of the below enemy)
+|| __**RUSTED MARAUDER**__
+:heart: : 6
+:moneybag: : Safety Scissors or 6 gold
+Behavior: When this takes damage or an ally dies, gain __Empower__.
+—————————————————
+1-4   | **Knife** (+2 :game_die:) :boom: lowest HP
+5-8   | **Broken Fang** (+2 :game_die:) :stealthIcon: :twisted_rightwards_arrows: -4 :game_die: if you have :dart:
+9-10 | **Hone** (-2 :game_die:) Your next :boom: has :dart:||"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def factory_examine(self, ctx):
+        msg = """You take a moment to stop and examine the machines. It was hard to see in the dim light but with a little use of your flashlight you can clearly see that the parts scattered around here are Sentian parts. The parts are very old, probably some of the first models ever produced by AEROR corp., even the logo is out of date! Something triggers deep within the recesses of your mind, you mindlessly navigate to a backroom where one machine is still in perfect functioning order. This seems so familiar. The machine is perched in the ceiling and needles and surgery equipment arm its front like teeth. In the chair a smaller Sentian is sat limp with its chest open. Your curiosity gets the better of you as you walk around to the front. The Sentian's chest is open; a human heart is exposed inside. Wires interlock it into place. It beats slowly and meekly. As you try to collect your emotions you notice that a black rot has begun eating away at its lower aorta. Your own heartbeat quickens as the Sentian child looks up at you weakly. You panic and head for the door with a quick step when you are stopped by another taller Sentian holding scissors and bandages.
+
+"We can't save him" it says to you, its eyes looking down at the bandages, "this was his favorite, take it with you to defeat the Evil One." it looks you in your eyes as it holds out a custom made forearm plate. You look down at the equipment. (You may spend 3 :heart: and 7 :gold: to upgrade your Sentian passive to: __AEROR Corp. Classic__ :gear: "Gain __Empower__ for each point of damage you take" lasts for the rest of this run)"""
         await ctx.send(msg)
 
     @commands.command()
