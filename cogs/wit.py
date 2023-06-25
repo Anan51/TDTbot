2c020e726a5f7ec14293fd4c4037d49b2f52f79d
StellarNemesis <come@mebro.email>
2023-06-24 21:45:11 -0400

    Added 3rd underwash_boss

--- cogs/wit.py
+++ cogs/wit.py
@@ -933,6 +933,16 @@ Behavior: Summon another Legion of Flesh x :busts_in_silhouette: x 4. They roll/
 8-10 | **Greed** (:game_die: - 3) If this took no damage, summon another Legion of Flesh
 11+    | **Pride** roll `!r 1d<# of Legion>`, That corresponding Legion is invincible until another dies
 https://www.youtube.com/watch?v=zP_1e30FWsE""",
+            """__**ITS RIGHT HAND**__
+:heart: : 20
+:moneybag: : 3d10 :test_tube:, +6 Comp Points
+Behavior: All players gain __Will__ if this takes any form of damage from a player, per damage taken.
+—————————————————
+1-3   | **Reaching** (:game_die: -3) :boom::boom::dart: to ALL players :twisted_rightwards_arrows: this gains -:heart::heart:
+4-6   | **Pulling** :shield::shield::shield: :twisted_rightwards_arrows: this gains -:heart::heart:
+7-10 | **Whispering** (:game_die: +2) If this took no damage, this gains -:heart::heart: and gains __empower__ x2
+11+    | **Sinking** all players must do a different encounter from underwash or the reach. This shares your encounter results. Combat is paused until you return.
+https://www.youtube.com/watch?v=EKLWC93nvAU""",
         ]
         await ctx.send(random.choice(encounters))
 
