ff40a47a99bfad12ebeb65782a5d41b2332d3ab7
StellarNemesis <come@mebro.email>
2023-07-27 09:47:59 -0400

    Added missing HYPNOCYTE

--- cogs/wit.py
+++ cogs/wit.py
@@ -997,6 +997,15 @@ Behavior: -m3:4Pa62sl
 5-8   | **Cursed Shield** :boom::boom::boom: :twisted_rightwards_arrows: +:game_die:
 9-10 | **Doubt** (+3 :game_die:) Gain __Burn__x3
 https://www.youtube.com/watch?v=qMX3aVbNdvo""",
+            """__**HYPNOCYTE**__
+:heart: : 8
+:moneybag: : Lose 3 __Will__
+Behavior: If this is alone, flee and yield no loot.
+—————————————————
+1-6   | **Indulge** (:game_die: + __will__ ) -2:heart: a different enemy :twisted_rightwards_arrows: this gains __will__ x2
+4-7   | **Control** (-3 :game_die:) Give all enemies __Empower__, +:heart:, and -:game_die: per every __Will__ this has
+8-10 | **Beckon** cause __Will__ per damage taken
+https://www.youtube.com/watch?v=H1Fx6rs66yM""",
             """__**ROTTING BEHEMOTH**__
 :heart: : 20
 :moneybag: : Lose 2 __Will__
