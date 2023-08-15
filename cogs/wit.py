35a0fced2e04d755274bae72b6caa6c5d22fdfac
StellarNemesis <come@mebro.email>
2023-08-15 11:33:34 -0400

    added gates_boss

--- cogs/wit.py
+++ cogs/wit.py
@@ -1246,6 +1246,22 @@ https://www.youtube.com/watch?v=ze0Rk-m0w2A""",
         await ctx.send(random.choice(encounters))
 
     @commands.command()
+    async def gates_boss(self, ctx):
+        encounters = [
+            """__**MALOKAI, PRINCE OF THE WORLD**__
+:heart: : 20 x :busts_in_silhouette:
+:moneybag: : "You cannot destroy me. You are allowed to only injure me. Take your weapon and leave me." 1d20 :dagger: {OR} If you have no __will__: ||summon tdt$gates_boss_2||
+Behavior: Cause __Will__ at the beginning of every player turn. Lowest HP player rolls move decisions
+—————————————————
+1-12    | **Nightmare Blade** :boom::boom::boom::shield::cyclone:; If this took no damage :twisted_rightwards_arrows: :boom::boom::boom::dart: Lowest HP next turn
+13-16   | **Soul Devour** :shield:, Cause __Will__ per damage taken :twisted_rightwards_arrows: Double the efficacy of your last turn
+17-19   | **Subtlety** :shield:, Summon another player. The Highest HP player must 1v1 them in #colosseum_1. If they refuse: lower the Levels and Comp Points by 3. If they lose lower their Level and Comp Points by 1. If they win: give them 10 Comp Points and 300XP. If the Highest HP Player wins: refill your :heart: and :large_blue_diamond:. Fights happen separately from this one.
+20       | **Submission** All players with 5 or more __will__ attack themselves this turn.
+https://www.youtube.com/watch?v=GGhZsiRCvYc""",
+        ]
+        await ctx.send(random.choice(encounters))
+
+    @commands.command()
     async def champions_landing_enemy(self, ctx):
         encounters = [
             """__**GELLY**__
