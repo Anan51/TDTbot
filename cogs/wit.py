7e59c03a609bdb63d6bb4cde8935cc068925806b
StellarNemesis <come@mebro.email>
2023-08-15 11:30:26 -0400

    music update for gates_enemy

--- cogs/wit.py
+++ cogs/wit.py
@@ -1157,7 +1157,7 @@ Behavior: When this takes damage, attacker loses 1 :large_blue_diamond:
 1-3   | **Crush** in 2 turns from now :boom::boom::boom::boom:
 4-8   | **Accretion** Gain persistent :shield: per damage taken this turn
 9-10 | **Singularity** (-2 :game_die:) in 4 turns from now, deal :boom::boom::boom::boom::boom::boom::dart::no_entry_sign: to ALL :twisted_rightwards_arrows: cause __Will__ x3
-https://www.youtube.com/watch?v=FhS2jvMDPv4""",
+https://www.youtube.com/watch?v=6KeLZodzA1I""",
             """__**∅ HUNTER**__
 :heart: : 15
 :moneybag: : !r 3d10 :gold:
