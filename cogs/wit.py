06971104b3096e4babec10580a28e845d6ab0afd
StellarNemesis <come@mebro.email>
2023-08-28 13:05:45 -0400

    Added gates boss stuff

--- cogs/wit.py
+++ cogs/wit.py
@@ -1401,6 +1401,100 @@ https://www.youtube.com/watch?v=GGhZsiRCvYc""",
         await ctx.send(random.choice(encounters))
 
     @commands.command()
+    async def gates_boss_2(self, ctx):
+        msg = """"So... you found the Sword." Malokai strokes his chin and leans forward "now who gave you that, hmmm?" He rests back into his throne, "you actually want to kill me? to "win"?... well I love to be the bearer of bad news, but I am immortal. No weapon fastened against me will succeed. But... you have potential... why don't you stop wasting your time against me and achieve that potential?" You can feel him tugging at your mind "I offer you unlimited Comprehension Points in exchange for you to end your life... and not to worry, you will have all of those on your next character." He stands up a pair of black and red wings unfurl out from his back "All you need to do is simply bow."
+
+**Surrender** tdt$malokai_surrender
+**Defy** tdt$malokai_defy
+:hourglass:: ||**Threaten** tdt$malokai_threaten||
+
+-----------------
+tdt$malokai_surrender
+
+Your hands are shaking. You wonder for a second if a full character reset would be worth the gold and Comp Points, would he even keep his word? Malokai is surely a liar right? Reading this dialog doesn't fill you with confidence either. "Did I just get tricked?" you think to yourself "Did I just throw away this character?" or maybe its simpler than that; "maybe" you think "I can get another character fast enough for it not to matter" but now having read this paragraph you wonder if Malokai was aware of this when making this offer but glancing down you see that it holds true. Infinite Comp Points at the cost of this character and a permanent stack of __will__, you feel like you kinda knew something like that was going to happen. Was it worth it? Maybe. Or maybe you have come back to this option just to see after trying and failing against Malokai's true form... Maybe this is desperation for some kind of answer... you wont know now. Your character, no matter how serious or goofy it was, kneels down before the prince. Immediately a dark force envelops your limbs crawling up along your spine and onto your face. There is a certain warmness to it. Its pleasant. You could just stay like this forever... infinite pleasure and infinite bliss. Is this what the others felt when they succumbed to __will__? (Delete this character from the game. Make a new character and give it infinite Comp Points and a permanent stack of __will__ that cannot be removed)
+
+---------------
+tdt$malokai_defy
+
+You draw your weapon and face down the dark prince.
+He sighs, "You understand you cannot win this fight, right? I will keep living. You WANT me to keep living. Without me the whole game is over. Nothing to chase. Nothing to work towards. I am an idea, a manifestation of Sin. Without me you have no purpose. This silly little game loses the drive. Look at you, you are playing a game that THOUSANDS of people see and ignore. You are an outcast. Without me this whole game is a waste of time. This excuse for you to live vicariously through the actions of your character, to have a different identity, a different sexuality, a different moral code: gone. To pretend the thoughts and actions of your character are not your own, gone. To Pretend that somehow who you are here is not a reflection of who you are in person: gone. Maybe you embrace that. You try to push out the nagging in the back of your mind that your life will be like a pea under the wheel of the universe. Think about it, I am your meaning. I am a goal simply to be attacked but never truly destroyed, because without me you might have to go back to the real world. To think about the consequences of your actions. And no one, not even He, can tell you what to do right? (either use tdt$malokai_surrender or tdt$gates_boss_3)
+
+--------------
+tdt$malokai_threaten
+
+The wind howls through the castle as Malokai sees who stands before him, "So you understand. You understand that you cannot win in your own power." Malokai seems frightened. "Fine then." (tdt$gates_boss_4)"""
+        await ctx.send(msg)
+
+    @commands.command()
+    async def gates_boss_3(self, ctx):
+        encounters = [
+            """"You really dont get it do you? I cannot be killed. They wont let me. I understand my role in this universe, why dont you?"
+
+__**MALOKAI, DRAGON OF WIT**__
+:heart: : 180 x :busts_in_silhouette:
+:moneybag: : tdtgates_boss_3
+Behavior: This causes __Will__ every minute this is alive
+—————————————————
+∞  | **Nightmare Blaze** tdt$malokai_attack
+https://youtu.be/eUcThHVbrXY""",
+            """"Feeling lucky are we? Or do you seek meaning in this encounter? Looking for some kind of secret"
+
+__**MALOKAI, DRAGON OF WIT**__
+:heart: : 180 x :busts_in_silhouette:
+:moneybag: : tdtgates_boss_3
+Behavior: This causes __Will__ every minute this is alive
+—————————————————
+∞  | **Nightmare Blaze** tdt$malokai_attack
+https://youtu.be/eUcThHVbrXY""",
+            """"Behold my reward. Look upon my permanency and tremble"
+
+__**MALOKAI, DRAGON OF WIT**__
+:heart: : 180 x :busts_in_silhouette:
+:moneybag: : tdtgates_boss_3
+Behavior: This causes __Will__ every minute this is alive
+—————————————————
+∞  | **Nightmare Blaze** tdt$malokai_attack
+https://youtu.be/eUcThHVbrXY""",
+            """"Foolish player. Do you not understand that you are expected to die? I am immortal. I am death."
+
+__**MALOKAI, DRAGON OF WIT**__
+:heart: : 180 x :busts_in_silhouette:
+:moneybag: : tdtgates_boss_3
+Behavior: This causes __Will__ every minute this is alive
+—————————————————
+∞  | **Nightmare Blaze** tdt$malokai_attack
+https://youtu.be/eUcThHVbrXY""",
+            """"Surrender is the reward you seek. When you fall here why not try it?"
+
+__**MALOKAI, DRAGON OF WIT**__
+:heart: : 180 x :busts_in_silhouette:
+:moneybag: : tdtgates_boss_3
+Behavior: This causes __Will__ every minute this is alive
+—————————————————
+∞  | **Nightmare Blaze** tdt$malokai_attack
+https://youtu.be/eUcThHVbrXY""",
+            ]
+        await ctx.send(random.choice(encounters))
+
+    @commands.command()
+    async def malokai_attack(self, ctx):
+        attacks = [
+            """**EDEN**: Remove all Modifiers from players :shield: and :boom: for the next turns per each player's __will__""",
+            """**ORION**: :boom::boom::cyclone: repeat per __will__ each player has""",
+            """**ANGUS**: :shield::shield::shield::shield::shield: ignores :no_entry_sign:, per __will__ the players have total""",
+            """**VIKTOR**: Delete the player with the highest __will__'s last turn. Remove any affect it may have had to Malokai this turn but deal any affect that may have done from Malokai. """,
+            """**NAYA**: Your previous turn also dealt :boom::zap::no_entry_sign: to the lowest HP player per __will__ they had""",
+            """**CONSTANTINE**: Gain __empower__, __protect__, and __healing__ per __will__ the players have in total. """,
+            """**EMBER**: Cause __weak__, __vulnerable__, and __burning__ to each player per __will__ the players have in total.""",
+            """**GAIDG-3**: Chose a random player, skip their turn per how much __will__ they have""",
+            """**YIN**: Double all players __Will__ if they have none: give all players 5 __will__""",
+            """**AEROR**: summon a gates enemy; break off from this fight to fight it but time keeps going here and so do other allies""",
+            """**REMUS**: :boom::shield: :twisted_rightwards_arrows: cause __will__ per success""",
+            """**FINCH**: Absorb the highest __will___ player's move used this turn. They cannot use it until this gets used again""",
+            ]
+        await ctx.send(random.choice(attacks))
+
+    @commands.command()
     async def champions_landing_enemy(self, ctx):
         await ctx.send(random.choice(_champions_landing_enemies))
 
